859996eb9d9f86dd333c57110bc1430c8c021239
“arisa-hirata” <arisa.pirata@gmail.com>
2019-08-15 11:23:59 -0700

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
