9fcf5a205db4ff227350c22a77d7867edfbd79bf
“arisa-hirata” <arisa.pirata@gmail.com>
2019-08-31 13:45:54 -0700

    implemented firebase utils, included ability to store authenticated users into the firestore database

--- src/App.js
+++ src/App.js
@@ -26,19 +26,16 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            },
-            () => {
-              console.log(this.state);
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
             }
-          );
+          });
+          console.log(this.state);
         });
       }
+      this.setState({ currentUser: userAuth });
     });
   }
 
