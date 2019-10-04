765167fce2599249901648b1b5b9deb7e59bf214
“arisa-hirata” <arisa.pirata@gmail.com>
2019-10-04 09:44:43 -0700

    updated images, logo and fonts

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,33 +1,38 @@
 const INITIAL_STATE = {
   sections: [
     {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'hats',
+      imageUrl:
+        'https://awesomestuff365.com/wp-content/uploads/2017/07/Handmade-Dog-Hats-7.jpg?x90917',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2016/01/11/10/18/dog-1133316_1280.jpg',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'shoes',
+      imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/41c-pijK6ML._SX425_.jpg',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'dogs',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2018/12/19/07/14/animal-3883568_1280.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'cats',
+      imageUrl:
+        'https://www.poponeko.jp/feature/wp-content/uploads/2019/04/1-99-1.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -6,111 +6,128 @@ const SHOP_DATA = {
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        name: 'Dog Sun Hat',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP._FODIQuwiFTwTlt8lAScAAHaJY&w=206&h=261&c=8&o=5&dpr=2&pid=1.7',
         price: 25
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        name: 'Brown CowBoy',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.4wWYg1Ast3UwvLPNJ-d4uAHaIM&w=206&h=228&c=8&o=5&dpr=2&pid=1.7',
         price: 18
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+        name: 'Pink Knit',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.qRLSP07-KDj3cKRtmzKvrwHaJT&w=206&h=259&c=8&o=5&dpr=2&pid=1.7',
         price: 35
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        name: 'Gentle Man',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.UU3ue5APNUuGZ2baGdBxCQHaLO&w=198&h=300&c=8&o=5&dpr=2&pid=1.7',
         price: 25
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+        name: 'Cuty Ribbon',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.aBQ745r6rN6TNoA3R8dfcQHaKL&w=206&h=283&c=8&o=5&dpr=2&pid=1.7',
         price: 18
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+        name: 'Christmas Tree',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.ux1a95rJy18tCAQStwYBEAHaJ8&w=206&h=277&c=8&o=5&dpr=2&pid=1.7',
         price: 14
       },
       {
         id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+        name: 'Birthday Cake',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.SR4IKclkAt-WYiYl8ILTNgHaJ4&w=206&h=275&c=8&o=5&dpr=2&pid=1.7',
         price: 18
       },
       {
         id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+        name: 'Cat Avitator',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.jiP8cHpFZg-BN9uvyUXJ_wHaJ4&w=206&h=275&c=8&o=5&dpr=2&pid=1.7',
         price: 14
       },
       {
         id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+        name: 'Minion',
+        imageUrl:
+          'https://www.bing.com/th?id=OIP.jPkxdJWQM6xv-JvA1KikqQHaHa&w=206&h=206&c=8&o=5&dpr=2&pid=1.7',
         price: 16
       }
     ]
   },
   sneakers: {
     id: 2,
-    title: 'Sneakers',
+    title: 'Shoes',
     routeName: 'sneakers',
     items: [
       {
         id: 1,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        name: 'Mesh Shoes',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/61Xw%2BQTBiFL._SX425_.jpg',
         price: 220
       },
       {
         id: 2,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+        name: 'Soft & Breathable',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/710ykj1nBqL._SX425_.jpg',
         price: 280
       },
       {
         id: 3,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+        name: 'Paw Protector',
+        imageUrl:
+          'https://p.globalsources.com/IMAGES/PDT/BIG/639/B1157094639.jpg',
         price: 110
       },
       {
         id: 4,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+        name: 'Sandals',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/718jlnTUQtL._SX425_.jpg',
         price: 160
       },
       {
         id: 5,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+        name: 'Slippers',
+        imageUrl:
+          'https://odditymall.com/includes/content/upload/shoe-shaped-dog-bed-2440.jpg',
         price: 160
       },
       {
         id: 6,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
+        name: 'Non Srip Warm Boots',
+        imageUrl:
+          'https://i5.walmartimages.com/asr/945123f9-fe25-449f-a7c0-7770d2dd72a4_1.b6d5498fe0a9ad15abc994e76f820c1d.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
         price: 160
       },
       {
         id: 7,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
+        name: 'Denim Converse',
+        imageUrl:
+          'https://thesolerevival.com/wp-content/uploads/2017/11/anticrueltysociety-dogshoes5.jpg',
         price: 190
       },
       {
         id: 8,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
+        name: 'Puppy Shoes',
+        imageUrl:
+          'https://cdn11.bigcommerce.com/s-t1ww8z0rje/images/stencil/600x600/products/1383/5445/TB2hGjjaFXXXXbbXpXXXXXXXXXX_704407379__24749.1541642719.jpg?c=2',
         price: 200
       }
     ]
@@ -122,124 +139,142 @@ const SHOP_DATA = {
     items: [
       {
         id: 1,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        name: 'Pink Jacket',
+        imageUrl:
+          'https://globalpetsonjasperedmonton.com/wp-content/uploads/2018/10/6801_WEB_800x.jpg',
         price: 125
       },
       {
         id: 2,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        name: 'Hunting Jacket',
+        imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1k7DvZX_6YH8Z1DUDINVdZCvDIOwH9yEL7HCZn04XAawRO8cHNw',
         price: 90
       },
       {
         id: 3,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        name: 'Lether Jacket',
+        imageUrl:
+          'http://www.lambupholstery.co.uk/images/cate_25/640/Winter-Dog-Clothes-Leather-Pet-Dog-Jacket-Warm-Pets-Dogs-Clothing-French-Bulldog-Puppy-Costume-Winter-Pet-Dogs-Coat-Chihuahua-Dog-Coats-Jackets-uhj0.jpg',
         price: 90
       },
       {
         id: 4,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+        name: 'Gray Hoodie',
+        imageUrl:
+          'https://s7d2.scene7.com/is/image/PetSmart/5291622?$sclp-prd-main_large$',
         price: 165
       },
       {
         id: 5,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
+        name: 'Rain Jacket',
+        imageUrl:
+          'https://i.etsystatic.com/20414605/d/il/f66a7d/2018174767/il_340x270.2018174767_4aqf.jpg?version=0',
         price: 185
       }
     ]
   },
   womens: {
     id: 4,
-    title: 'Womens',
+    title: 'Dogs',
     routeName: 'womens',
     items: [
       {
         id: 1,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+        name: 'Hoodie',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2017/06/20/15/57/dog-2423578__480.png',
         price: 25
       },
       {
         id: 2,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+        name: 'Check Sweater',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2017/08/05/12/44/dog-2583282__480.jpg',
         price: 20
       },
       {
         id: 3,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+        name: 'Winter Jacket',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2016/01/11/10/11/dog-1133311__480.jpg',
         price: 80
       },
       {
         id: 4,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+        name: 'Pink Shirt',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2017/06/20/15/58/dog-2423581__480.png',
         price: 80
       },
       {
         id: 5,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+        name: 'Border Sweater',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2017/01/25/12/02/boston-terrier-2007709__480.jpg',
         price: 45
       },
       {
         id: 6,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+        name: 'Yellow Suit',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2019/02/11/20/24/yorkshire-terrier-3990685__480.jpg',
         price: 135
       },
       {
         id: 7,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+        name: 'Blue Sweater',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2014/08/05/20/47/pug-410982__480.jpg',
         price: 20
       }
     ]
   },
   mens: {
     id: 5,
-    title: 'Mens',
+    title: 'Cats',
     routeName: 'mens',
     items: [
       {
         id: 1,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        name: 'Maid Cat',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/51boekD0vuL._SY355_.jpg',
         price: 325
       },
       {
         id: 2,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+        name: 'Yukata',
+        imageUrl:
+          'https://images-fe.ssl-images-amazon.com/images/I/51Z70ROhWYL.jpg',
         price: 20
       },
       {
         id: 3,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+        name: 'Fluffy Hoodie',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/51d0xTSn3jL._SY355_.jpg',
         price: 25
       },
       {
         id: 4,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+        name: 'Ninja Suit',
+        imageUrl:
+          'https://item-shopping.c.yimg.jp/i/j/petio-online-shop_w25715',
         price: 25
       },
       {
         id: 5,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+        name: 'CowBoy',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/61y3z6k6mhL._SY355_.jpg',
         price: 40
       },
       {
         id: 6,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+        name: 'Samurai Cat',
+        imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/51yA-LEXaoL._SY355_.jpg',
         price: 25
       }
     ]
