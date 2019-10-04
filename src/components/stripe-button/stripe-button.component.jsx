3a96d50d86dc9ac828ca619347213b96afaaf4ca
“arisa-hirata” <arisa.pirata@gmail.com>
2019-10-03 21:05:14 -0700

    added stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_0NBn8GNzR0C7d03GfNb6dQF700qTWEaqyB';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="PawPaw Closhing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://arisahirata.com/img/headerLogo.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
